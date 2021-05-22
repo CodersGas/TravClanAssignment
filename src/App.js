9eafd82abb5f5c015e98ae01e828ab6876e36de6
Ashish Kumar <rudrashishkumar@gmail.com>
2021-05-22 17:12:26 +0530

    Handing API faliure scenario. If API fails to load data, user will see Retry Option

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import './App.css';
 import Layout from './components/layout';
 import connectAPI from './utils/api_endpoints';
 import MerchantsTable from './components/merchantsTable';
-import {Grid, Box} from '@material-ui/core';
+import {Grid, Box, Button, Typography} from '@material-ui/core';
 
 function App() {
 
   const [merchantsData, setMerchantsData] = useState(null);
+  const [err, setErr]                     = useState(false); 
 
   const callAPI = async() => {
     try{
       let response =  await connectAPI('getUsersList');
       setMerchantsData(response); 
     }catch(err) {
+      setErr(true);
       console.log('err while fetching merchants data -> ', err);
     }
   }
@@ -28,20 +30,35 @@ function App() {
         <Grid container justify='center' alignItems='center' >
           <Grid item md={10} xs={12} sm={12} >
             {
-              merchantsData &&
+              !err && merchantsData &&
               <MerchantsTable 
                 merchantsData={merchantsData}
               />
             }
 
             {
-              !merchantsData &&
+              !merchantsData && !err &&
               <Box display='flex' my={2} justifyContent='center' >
                 <img 
                   src={'./doubleRingLoader.gif'}
                   width="56px"
                   height="56px"
                 />
+              </Box>
+            }
+
+            {
+              err &&
+              <Box width={1} display='flex' justifyContent='center' alignItems='center' flexDirection='column'>
+                <Typography>
+                  Oops! Something went wrong. Please try again
+                </Typography>
+
+                <Box my={2} >
+                  <Button onClick={callAPI} variant="outlined" color='primary' >
+                    Retry
+                  </Button>
+                </Box>
               </Box>
             }
           </Grid>
